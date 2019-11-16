eb4591ca52ec45246851df0238b62328d2787598
djwilck <wil17156@byui.edu>
2019-11-16 14:43:15 -0700

    Update

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -13,24 +13,24 @@ fetch(requestURL)
                 let article = document.createElement('article');
                 let text = document.createElement('section');
                 let h3 = document.createElement('h3');
-                let h5 = document.createElement('h5');
+                let h4 = document.createElement('h4');
                 let p1 = document.createElement('p');
                 let p2 = document.createElement('p');
                 let p3 = document.createElement('p');
                 let image = document.createElement('img');
 
                 h3.textContent = towns[i].name;
-                h5.textContent = towns[i].motto;
-                h5.className = "italic"
+                h4.textContent = towns[i].motto;
+                h4.className = "italic"
                 p1.textContent = "Year Founded: " + towns[i].yearFounded;
                 p2.textContent = "Population: " + towns[i].currentPopulation;
                 p3.textContent = "Annual Rain Fall: " + towns[i].averageRainfall + "in.";
                 image.setAttribute('src', "images/" + towns[i].photo);
-                image.setAttribute('alt', "Image of " + towns[i].name);
+                image.setAttribute('alt', "Image of " + towns[i].name + ", Idaho");
 
 
                 text.appendChild(h3);
-                text.appendChild(h5);
+                text.appendChild(h4);
                 text.appendChild(p1);
                 text.appendChild(p2);
                 text.appendChild(p3);
